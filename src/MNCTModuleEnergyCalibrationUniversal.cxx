4113bf16f7b5bcf1fd7dc7ebc4c8e71792a44204
Andreas Zoglauer <zoglauer@berkeley.edu>
2014-08-19 02:21:42 +0000

    CHG: Modified the reader to include later outputs of melinator

--- src/MNCTModuleEnergyCalibrationUniversal.cxx
+++ src/MNCTModuleEnergyCalibrationUniversal.cxx
@@ -120,85 +120,91 @@ MNCTModuleEnergyCalibrationUniversal::~MNCTModuleEnergyCalibrationUniversal()
 bool MNCTModuleEnergyCalibrationUniversal::Initialize()
 {
   // Initialize the module 
-
+  
   MParser Parser;
   if (Parser.Open(m_FileName, MFile::c_Read) == false) {
     mout<<"Unable to open file "<<m_FileName<<endl;
     return false;
   }
-
+  
+  cout<<"#1"<<endl;
+  
+  map<MReadOutElementDoubleStrip, unsigned int> CP_ROEToLine;
+  map<MReadOutElementDoubleStrip, unsigned int> CM_ROEToLine;
   for (unsigned int i = 0; i < Parser.GetNLines(); ++i) {
-    unsigned int Pos = 0;
     unsigned int NTokens = Parser.GetTokenizerAt(i)->GetNTokens();
-    if (NTokens == 0) continue;
-    if (Parser.GetTokenizerAt(i)->IsTokenAt(Pos, "CP") == false) continue;
-    
-    // Check the calibration points at this stage. This would be better if we just looked at the number of lines fit through melinator to make sure there will be a good fit resulting, but for now, I've left it as is...
-
-    // Read the read out element (I changed very little in the below code. Just updated the ReadOutElements corresponding to the correct Pos.
-    ++Pos;
-    if (NTokens <= 15) {
-      cout<<m_XmlTag<<": "<<Parser.GetTokenizerAt(i)->GetText()<<endl
-          <<"Line parser: Not enough tokens for a calibration point (failed at read-out element)"<<endl;
-      //return false;
-    }
-    MReadOutElement* ReadOutElement = 0;
-    MString ReadOutElementString = Parser.GetTokenizerAt(i)->GetTokenAtAsString(Pos);
-    ReadOutElementString.ToLower();
-    if (ReadOutElementString == "dss") {
-      ++Pos;
-      MReadOutElementDoubleStrip* R = new MReadOutElementDoubleStrip();
-      R->SetDetectorID(Parser.GetTokenizerAt(i)->GetTokenAtAsUnsignedInt(Pos));
-      ++Pos;
-      R->SetStripID(Parser.GetTokenizerAt(i)->GetTokenAtAsUnsignedInt(Pos));
-      ++Pos;
-      R->IsPositiveStrip(Parser.GetTokenizerAt(i)->GetTokenAtAsString(Pos) == "p");
-      ReadOutElement = R;
-    } else {
-      cout<<m_XmlTag<<": "<<Parser.GetTokenizerAt(i)->GetText()<<endl
-          <<"Line parser: Unknown read out element: "<<ReadOutElementString<<endl;
-      return false;
-    }
-    
-    
-    // Read the calibrator, i.e. read the fit function from the .ecal Melinator file.
-
-    ++Pos;
-    MString CalibratorType = Parser.GetTokenizerAt(i+1)->GetTokenAtAsString(Pos);
-    CalibratorType.ToLower();
-
-    //Eventually, I'll be including other possible fits, but for now, let's just get this working with poly3...
-    if (CalibratorType == "poly3") {
-        ++Pos;
-	double a0 = Parser.GetTokenizerAt(i+1)->GetTokenAtAsDouble(Pos);
-	++Pos;
-	double a1 = Parser.GetTokenizerAt(i+1)->GetTokenAtAsDouble(Pos);
-	++Pos;
-	double a2 = Parser.GetTokenizerAt(i+1)->GetTokenAtAsDouble(Pos);
-	++Pos;
-	double a3 = Parser.GetTokenizerAt(i+1)->GetTokenAtAsDouble(Pos);
-
-	//From the fit parameters I just extracted from the .ecal file, I can define a function
-	TF1 * melinatorfit = new TF1("poly3","[0]+[1]*x+[2]*x^2+[3]*x^3",0.,8000.);
-	melinatorfit->FixParameter(0,a0);
-	melinatorfit->FixParameter(1,a1);
-	melinatorfit->FixParameter(2,a2);
-	melinatorfit->FixParameter(3,a3);
-
-	//Define the map by saving the fit function I just created as a map to the current ReadOutElement
-	m_Calibration[*dynamic_cast<MReadOutElementDoubleStrip*>(ReadOutElement)] = melinatorfit;;
-
-
-	//And, closing the "if poly3" statemnet...
-    } else if (CalibratorType == "pakw") {
-      
-      // Ignore
-    } else {
-      cout<<m_XmlTag<<": "<<Parser.GetTokenizerAt(i)->GetText()<<endl
-          <<"Line parser: Unknown calibrator type: "<<CalibratorType<<endl;
-      return false;
+    if (NTokens < 2) continue;
+    if (Parser.GetTokenizerAt(i)->IsTokenAt(0, "CP") == true ||
+      Parser.GetTokenizerAt(i)->IsTokenAt(0, "CM") == true) {
+      if (Parser.GetTokenizerAt(i)->IsTokenAt(1, "dss") == true) {
+        
+        MReadOutElementDoubleStrip R;
+        R.SetDetectorID(Parser.GetTokenizerAt(i)->GetTokenAtAsUnsignedInt(2));
+        R.SetStripID(Parser.GetTokenizerAt(i)->GetTokenAtAsUnsignedInt(3));
+        R.IsPositiveStrip(Parser.GetTokenizerAt(i)->GetTokenAtAsString(4) == "p");
+        if (Parser.GetTokenizerAt(i)->IsTokenAt(0, "CP") == true) {
+          CP_ROEToLine[R] = i;
+        } else {
+          CM_ROEToLine[R] = i;
+        }
+      } else {
+        cout<<m_XmlTag<<": Line parser: Unknown read out element ("<<Parser.GetTokenizerAt(i)->GetTokenAt(1)<<")"<<endl;
+        return false;
+      }
     }
   }
+  
+  cout<<"#2"<<endl;
+  
+  for (auto CM: CM_ROEToLine) {
+    // If we have at least three data points, we store the calibration
+    cout<<"Looking at "<<CM.first<<endl;
+    
+    if (CP_ROEToLine.find(CM.first) != CP_ROEToLine.end()) {
+      unsigned int i = CP_ROEToLine[CM.first];
+      if (Parser.GetTokenizerAt(i)->IsTokenAt(5, "pakw") == true) {
+        if (Parser.GetTokenizerAt(i)->GetTokenAtAsInt(6) < 3) {
+          cout<<m_XmlTag<<": Not enough calibration points (only "<<Parser.GetTokenizerAt(i)->GetTokenAtAsInt(6)<<") for strip: "<<CM.first<<endl;
+          continue;
+        }
+      } else {
+        cout<<m_XmlTag<<": Unknown calibration point descriptor found: "<<Parser.GetTokenizerAt(i)->GetTokenAt(5)<<endl;
+        continue;
+      }
+    } else {
+      cout<<m_XmlTag<<": No good calibration for the following strip found: "<<CM.first<<endl;
+      continue;
+    }
+    
+    // Read the calibrator, i.e. read the fit function from the .ecal Melinator file.
+    
+    unsigned int Pos = 5;
+    MString CalibratorType = Parser.GetTokenizerAt(CM.second)->GetTokenAtAsString(Pos);
+    CalibratorType.ToLower();
+    
+    //Eventually, I'll be including other possible fits, but for now, let's just get this working with poly3...
+    if (CalibratorType == "poly3") {
+      double a0 = Parser.GetTokenizerAt(CM.second)->GetTokenAtAsDouble(++Pos);
+      double a1 = Parser.GetTokenizerAt(CM.second)->GetTokenAtAsDouble(++Pos);
+      double a2 = Parser.GetTokenizerAt(CM.second)->GetTokenAtAsDouble(++Pos);
+      double a3 = Parser.GetTokenizerAt(CM.second)->GetTokenAtAsDouble(++Pos);
+      
+      //From the fit parameters I just extracted from the .ecal file, I can define a function
+      TF1* melinatorfit = new TF1("poly3","[0]+[1]*x+[2]*x^2+[3]*x^3",0.,8000.);
+      melinatorfit->FixParameter(0, a0);
+      melinatorfit->FixParameter(1, a1);
+      melinatorfit->FixParameter(2, a2);
+      melinatorfit->FixParameter(3, a3);
+      
+      //Define the map by saving the fit function I just created as a map to the current ReadOutElement
+      m_Calibration[CM.first] = melinatorfit;;
+      
+    } else {
+      cout<<m_XmlTag<<": Line parser: Unknown calibrator type ("<<CalibratorType<<") for strip"<<CM.first<<endl;
+      continue;
+    }
+  }
+  
   return true;
 }
 
@@ -216,7 +222,7 @@ bool MNCTModuleEnergyCalibrationUniversal::AnalyzeEvent(MNCTEvent* Event)
 
     TF1* Fit = m_Calibration[*dynamic_cast<MReadOutElementDoubleStrip*>(R)];
     if (Fit == 0) {
-      cout<<"Error: Fit not found for read-out element "<<&R<<endl;
+      cout<<"Error: Fit not found for read-out element "<<*R<<endl;
       Event->SetEnergyCalibrationIncomplete(true);
     } else {
       double Energy = Fit->Eval(SH->GetADCUnits());
