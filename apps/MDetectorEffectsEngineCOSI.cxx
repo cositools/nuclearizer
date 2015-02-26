58829accae02add078360df5f52c49263f02b651
Clio Sleator <sleator@berkeley.edu>
2015-02-26 23:02:32 +0000

    added energy to ADC conversion

--- apps/MDetectorEffectsEngineCOSI.cxx
+++ apps/MDetectorEffectsEngineCOSI.cxx
@@ -29,6 +29,7 @@ using namespace std;
 #include <TH1.h>
 #include <TCanvas.h>
 #include <MString.h>
+#include <TRandom3.h>
 
 // MEGAlib
 #include "MGlobal.h"
@@ -60,6 +61,8 @@ public:
   bool Analyze();
   //! Interrupt the analysis
   void Interrupt() { m_Interrupt = true; }
+	//! Read in and parse energy calibration file
+	void ParseEcalFile();
 
 private:
   //! True, if the analysis needs to be interrupted
@@ -69,7 +72,14 @@ private:
   MString m_FileName;
   //! Geometry file name
   MString m_GeometryFileName;
-  
+
+	//! Calibration map between read-out element and fitted function for energy calibration
+	map<MReadOutElementDoubleStrip, TF1*> m_EnergyCalibration;
+	//! Calibration map between read-out element and fitted function for energy resolution calibration
+	map<MReadOutElementDoubleStrip, TF1*> m_ResolutionCalibration;
+	//! Energy calibration file name
+	MString m_ecalFilename;
+
   //! Tiny helper class for MNCTDetectorEffectsEngineCOSI describing a special strip hit
   class MNCTDEEStripHit
   {
@@ -92,6 +102,9 @@ private:
     //! A list of original strip hits making up this strip hit
     vector<MNCTDEEStripHit> m_SubStripHits;
   };
+public:
+	//! Convert Energy to ADC value
+	int EnergyToADC(MNCTDEEStripHit& Hit, double energy);
 
   
 };
@@ -129,6 +142,7 @@ bool MNCTDetectorEffectsEngineCOSI::ParseCommandLine(int argc, char** argv)
   Usage<<"    General options:"<<endl;
   Usage<<"         -f:   simulation file name"<<endl;
   Usage<<"         -g:   geometry file name"<<endl;
+	Usage<<"         -e:   energy calibration file name"<<endl;
   Usage<<"         -h:   print this help"<<endl;
   Usage<<endl;
 
@@ -172,7 +186,10 @@ bool MNCTDetectorEffectsEngineCOSI::ParseCommandLine(int argc, char** argv)
     } else if (Option == "-g") {
       m_GeometryFileName = argv[++i];
       cout<<"Accepting file name: "<<m_GeometryFileName<<endl;
-    } else {
+    } else if (Option == "-e") {
+			m_ecalFilename = argv[++i];
+			cout << "Accepting energy calibration file name: " << m_ecalFilename << endl;
+		} else {
       cout<<"Error: Unknown option \""<<Option<<"\"!"<<endl;
       cout<<Usage.str()<<endl;
       return false;
@@ -217,6 +234,9 @@ bool MNCTDetectorEffectsEngineCOSI::Analyze()
     return false;
   }  
 
+	//load energy calibration information
+	ParseEcalFile();
+
   MFileEventsSim* Reader = new MFileEventsSim(Geometry);
   if (Reader->Open(m_FileName) == false) {
     cout<<"Unable to open sim file "<<m_FileName<<" - Aborting!"<<endl; 
@@ -233,7 +253,10 @@ bool MNCTDetectorEffectsEngineCOSI::Analyze()
   out<<"TYPE   ROA"<<endl;
   out<<"UF     doublesidedstrip adcwithtiming"<<endl;
   out<<endl;
-  
+
+//	TCanvas specCanvas("c","c",600,400); 
+//	TH1D* spectrum = new TH1D("spec","spectrum",40,640,680);
+ 
   MSimEvent* Event = 0;
   int RunningID = 0;
   while ((Event = Reader->GetNextEvent()) != 0) {
@@ -350,9 +373,13 @@ bool MNCTDetectorEffectsEngineCOSI::Analyze()
         Energy += SubHit.m_Energy;
       }
       
-      // Let's do a dummy energy:
       Hit.m_Energy = Energy;
-      Hit.m_ADC = int(10*Energy);
+			Hit.m_ADC = EnergyToADC(Hit,Energy);
+//			spectrum->Fill(Energy);
+//			cout << "Energy: " << Energy << '\t';
+//			cout << "ADC: " << Hit.m_ADC << endl;
+      // Let's do a dummy energy:
+//      Hit.m_ADC = int(10*Energy);
     }
     
     
@@ -412,6 +439,9 @@ bool MNCTDetectorEffectsEngineCOSI::Analyze()
     delete Event;
   }
 
+//	spectrum->Draw();
+//	specCanvas.Print("spectrum_adc.pdf");
+
   // Some cleanup
   out<<"EN"<<endl<<endl;
   out.close();
@@ -420,6 +450,141 @@ bool MNCTDetectorEffectsEngineCOSI::Analyze()
   
   return true;
 }
+
+/******************************************************************************/
+
+//convert energy to ADC value by reversing energy calibration done in MNCTModuleEnergyCalibrationUniversal.cxx
+int MNCTDetectorEffectsEngineCOSI::EnergyToADC(MNCTDEEStripHit& Hit, double mean_energy){
+
+	//first, need to simulate energy spread
+	TF1* FitRes = m_ResolutionCalibration[Hit.m_ROE];
+	//resolution is a function of energy
+	double EnergyResolution = 3; //default to 3keV...does this make sense?
+	if (FitRes != 0){
+		EnergyResolution = FitRes->Eval(mean_energy);
+	}
+
+	//get energy from gaussian around mean_energy with sigma=EnergyResolution
+	TRandom3 r(0);
+	double energy = r.Gaus(mean_energy,EnergyResolution);
+//	spectrum->Fill(energy);
+
+//	if (fabs(mean_energy-662.) < 5){
+//		cout << mean_energy << '\t' << EnergyResolution << '\t' << energy << endl;
+//	}
+
+	//then, convert energy to ADC
+	double ADC_double = 0;
+
+	//get the fit function
+	TF1* Fit = m_EnergyCalibration[Hit.m_ROE];
+
+	if (Fit != 0){
+		//find roots
+		ADC_double = Fit->GetX(energy,0.,8000.);
+	}
+
+
+	int ADC = int(ADC_double);
+	return ADC;
+
+};
+
+//parse ecal file: should be done once at the beginning to save all the poly3 coefficients
+void MNCTDetectorEffectsEngineCOSI::ParseEcalFile(){
+
+	MParser Parser;
+	if (Parser.Open(m_ecalFilename, MFile::c_Read) == false){
+		cout << "Unable to open calibration file " << m_ecalFilename << endl;
+		return;
+	}
+
+	map<MReadOutElementDoubleStrip, unsigned int> CM_ROEToLine;	//Energy Calibration Model
+	map<MReadOutElementDoubleStrip, unsigned int> CR_ROEToLine; //Energy Resolution Calibration Model
+	//used to make sure there are enough data points:
+	map<MReadOutElementDoubleStrip, unsigned int> CP_ROEToLine;	//peak fits
+
+	for (unsigned int i=0; i<Parser.GetNLines(); i++){
+		unsigned int NTokens = Parser.GetTokenizerAt(i)->GetNTokens();
+		if (NTokens < 2) continue;
+		if (Parser.GetTokenizerAt(i)->IsTokenAt(0,"CM")==true || Parser.GetTokenizerAt(i)->IsTokenAt(0,"CP")==true
+					|| Parser.GetTokenizerAt(i)->IsTokenAt(0,"CR")==true){
+
+			if (Parser.GetTokenizerAt(i)->IsTokenAt(1,"dss") == true){
+				MReadOutElementDoubleStrip R;
+				R.SetDetectorID(Parser.GetTokenizerAt(i)->GetTokenAtAsUnsignedInt(2));
+				R.SetStripID(Parser.GetTokenizerAt(i)->GetTokenAtAsUnsignedInt(3));
+				R.IsPositiveStrip(Parser.GetTokenizerAt(i)->GetTokenAtAsString(4) == "p");
+
+				if (Parser.GetTokenizerAt(i)->IsTokenAt(0,"CM") == true){
+					CM_ROEToLine[R] = i;
+				}
+				else if (Parser.GetTokenizerAt(i)->IsTokenAt(0,"CP") == true){
+					CP_ROEToLine[R] = i;
+				}
+				else {
+					CR_ROEToLine[R] = i;
+				}
+			}
+		}
+	}
+
+	for (auto CM: CM_ROEToLine){
+
+		//only use calibration if we have 3 data points
+		if (CP_ROEToLine.find(CM.first) != CP_ROEToLine.end()){
+			unsigned int i = CP_ROEToLine[CM.first];
+			if (Parser.GetTokenizerAt(i)->IsTokenAt(5,"pakw") == true){
+				if (Parser.GetTokenizerAt(i)->GetTokenAtAsInt(6) < 3){
+					continue;
+				}
+			}
+		}
+
+
+		//get the fit function from the file
+		unsigned int Pos = 5;
+		MString CalibratorType = Parser.GetTokenizerAt(CM.second)->GetTokenAtAsString(Pos);
+		CalibratorType.ToLower();
+
+		//for now Carolyn just does poly3, so I am only doing that one
+		if (CalibratorType == "poly3"){
+			double a0 = Parser.GetTokenizerAt(CM.second)->GetTokenAtAsDouble(++Pos);
+			double a1 = Parser.GetTokenizerAt(CM.second)->GetTokenAtAsDouble(++Pos);
+			double a2 = Parser.GetTokenizerAt(CM.second)->GetTokenAtAsDouble(++Pos);
+			double a3 = Parser.GetTokenizerAt(CM.second)->GetTokenAtAsDouble(++Pos);
+
+			//from fit parameters, define function
+			TF1* melinatorfit = new TF1("poly3","[0]+[1]*x+[2]*x^2+[3]*x^3",0.,8000.);
+			melinatorfit->FixParameter(0,a0);
+			melinatorfit->FixParameter(1,a1);
+			melinatorfit->FixParameter(2,a2);
+			melinatorfit->FixParameter(3,a3);
+
+			//Define the map by saving the fit function I just created as a map to the current ReadOutElement
+			m_EnergyCalibration[CM.first] = melinatorfit;
+		}
+	}
+
+	for (auto CR: CR_ROEToLine){
+
+		unsigned int Pos = 5;
+		MString CalibratorType = Parser.GetTokenizerAt(CR.second)->GetTokenAtAsString(Pos);
+		CalibratorType.ToLower();
+		if (CalibratorType == "p1"){
+			double f0 = Parser.GetTokenizerAt(CR.second)->GetTokenAtAsDouble(++Pos);
+			double f1 = Parser.GetTokenizerAt(CR.second)->GetTokenAtAsDouble(++Pos);
+			TF1* resolutionfit = new TF1("P1","([0]+[1]*x)^(1/2)",0.,2000.);
+			resolutionfit->FixParameter(0,f0);
+			resolutionfit->FixParameter(1,f1);
+
+			m_ResolutionCalibration[CR.first] = resolutionfit;
+		}
+	}
+
+};
+
+/******************************************************************************/
 
 
 /******************************************************************************/
