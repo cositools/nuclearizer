ec2d091239f0779c8c9a5d3ef189b2328a56901f
Andreas Zoglauer <zoglauer@berkeley.edu>
2016-08-16 20:40:07 +0000

    ADD: Crab reference image

---
+++ resource/examples/crabsanitycheck/analyze.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+GEO=`cat ~/.nuclearizer.cfg | grep Geo | awk -F">" '{ print $2 }' | awk -F"<" '{ print $1 }'`
+GEO=${GEO/\(/\{}; 
+GEO=${GEO/\)/\}};
+
+if [[ ${GEO} == "" ]]; then
+  echo "Unable to find geometry file. make sure you have set it in your .nuclearizer.cfg file"
+  exit;
+fi
+
+nuclearizer -a -c Day_160612.nuclearizer.cfg -g ${GEO} &
+nuclearizer -a -c Day_160613.nuclearizer.cfg -g ${GEO} &
+
+wait
+
+revan -a -f Day_160612.evta.gz -c Crab.revan.cfg -g ${GEO} &
+revan -a -f Day_160613.evta.gz -c Crab.revan.cfg -g ${GEO} &
+
+wait
+
+mimrec -c Crab.mimrec.cfg -f Crab.tra -i -o Crab.png
