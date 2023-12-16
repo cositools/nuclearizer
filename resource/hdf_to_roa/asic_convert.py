e1de26bfd659fc7a2e18ff598ae6b7904ec6cd5a
fieldrog <field.rogers@gmail.com>
2023-12-16 09:27:25 -0800

    ran black on python files

--- resource/hdf_to_roa/asic_convert.py
+++ resource/hdf_to_roa/asic_convert.py
@@ -9,7 +9,7 @@ from typing import Iterable, Tuple
 class Hdf:
     """Write and read acquired data in HDF format."""
 
-    def __init__(self, filename: str, mode: str = 'w') -> None:
+    def __init__(self, filename: str, mode: str = "w") -> None:
         """
         Create the filename for later opening.
 
@@ -20,10 +20,12 @@ class Hdf:
         """
         self.filename = filename
         self.mode = mode
-        self._header = np.zeros(1,
-                                np.dtype([('BufferType', np.uint32),
-                                          ('UTC', np.uint64),
-                                          ('Index', np.uint64)]))
+        self._header = np.zeros(
+            1,
+            np.dtype(
+                [("BufferType", np.uint32), ("UTC", np.uint64), ("Index", np.uint64)]
+            ),
+        )
 
     def __enter__(self) -> object:
         """Context open of the file."""
@@ -39,11 +41,9 @@ class Hdf:
         return self._hdf_file.root
 
     def create_table(self, name, dtype, title):
-        return self._hdf_file.create_table(self.root,
-                                           name,
-                                           dtype,
-                                           title=title,
-                                           filters=self.filters)
+        return self._hdf_file.create_table(
+            self.root, name, dtype, title=title, filters=self.filters
+        )
 
     def _open(self) -> None:
         """
@@ -56,18 +56,21 @@ class Hdf:
         from the ASIC DAQ.  Buffers hold the actual raw
         data from the instrument converted into <u32 integers.
         """
-        self.filters = tables.Filters(complevel=1, complib='zlib')
+        self.filters = tables.Filters(complevel=1, complib="zlib")
         self._hdf_file = tables.open_file(
-            self.filename, mode=self.mode, filters=self.filters)
-        if 'w' in self.mode:
-            self.buffers = self._hdf_file.create_vlarray(self.root,
-                                                         'Buffers',
-                                                         atom=tables.UInt32Atom(),
-                                                         title='Ragged array of raw data buffer',
-                                                         filters=self.filters)
-            self.headers = self.create_table('Headers',
-                                             self._header.dtype,
-                                             title='Buffer header data')
+            self.filename, mode=self.mode, filters=self.filters
+        )
+        if "w" in self.mode:
+            self.buffers = self._hdf_file.create_vlarray(
+                self.root,
+                "Buffers",
+                atom=tables.UInt32Atom(),
+                title="Ragged array of raw data buffer",
+                filters=self.filters,
+            )
+            self.headers = self.create_table(
+                "Headers", self._header.dtype, title="Buffer header data"
+            )
         else:
             self.buffers = self.root.Buffers
             self.headers = self.root.Headers
@@ -84,11 +87,11 @@ class Hdf:
         Append the binary data to file and put it into
         a uint32 format.
         """
-        self._header['BufferType'] = buffer_type
-        self._header['UTC'] = utc
-        self._header['Index'] = self.buffers.nrows
+        self._header["BufferType"] = buffer_type
+        self._header["UTC"] = utc
+        self._header["Index"] = self.buffers.nrows
         self.headers.append(self._header)
-        self.buffers.append(data.view('uint32'))
+        self.buffers.append(data.view("uint32"))
 
     def read(self):
         """Return all of the data at once."""
@@ -104,84 +107,87 @@ class Hdf:
         smaller elements.
         """
         header = self.headers[index]
-        data = self.buffers[header['Index']]
-        if header['BufferType'] == 12:  # ASIC data
-            data = data.view('>u2')
-        elif header['BufferType'] == 11:  # Bridgeport data
-            data = data.view('u2')
-        return header['BufferType'], header['UTC'], data
-
+        data = self.buffers[header["Index"]]
+        if header["BufferType"] == 12:  # ASIC data
+            data = data.view(">u2")
+        elif header["BufferType"] == 11:  # Bridgeport data
+            data = data.view("u2")
+        return header["BufferType"], header["UTC"], data
 
 
 class AsicConvert:
+    def __init__(self, file_glob, verbose=False):
+        self.dtype = np.dtype(
+            [
+                ("asic", "uint8"),
+                ("channel", "uint16"),
+                ("adc", "int16"),
+                ("tac", "int16"),
+                ("oscillator", "uint32"),
+                ("headers", "uint16"),
+                ("dt", "uint8"),
+            ]
+        )
+        # self.dtypedt = np.dtype([('dt','uint8')])
+        self.verbose = verbose
+        if self.verbose:
+            print(file_glob)
+        self.glob(file_glob)
 
-	def __init__(self, file_glob,verbose=False):
-		self.dtype = np.dtype([
-			('asic', 'uint8'),
-			('channel', 'uint16'),
-			('adc', 'int16'),
-			('tac', 'int16'),
-			('oscillator', 'uint32'),
-			('headers', 'uint16'),
-			('dt','uint8')
-		])
-		#self.dtypedt = np.dtype([('dt','uint8')])
-		self.verbose = verbose
-		if self.verbose: print (file_glob)
-		self.glob(file_glob)
+    def glob(self, file_glob):
+        if self.verbose:
+            print(len(file_glob))
+        if len(file_glob) == 0:
+            f_glob = sorted(glob("data/*.hdf"))
+        elif len(file_glob) == 1:
+            path = pathlib.Path(file_glob[0])
+            if path.is_dir():
+                # print("path.is_dir")
+                f_glob = sorted(glob(str(path / "*.hdf")))
+            elif path.is_file():
+                # print("path.is_file")
+                f_glob = [str(path)]
+            else:
+                # print("else")
+                f_glob = sorted([item for item in glob(str(path))])
+        else:
+            f_glob = sorted([item for name in file_glob for item in glob(name)])
 
-	def glob(self, file_glob):
-		if self.verbose: print(len(file_glob))
-		if len(file_glob) == 0:
-			f_glob = sorted(glob('data/*.hdf'))
-		elif len(file_glob) == 1:
-			path = pathlib.Path(file_glob[0])
-			if path.is_dir():
-				#print("path.is_dir")
-				f_glob = sorted(glob(str(path / '*.hdf')))
-			elif path.is_file():
-				#print("path.is_file")
-				f_glob = [str(path)]
-			else:
-				#print("else")
-				f_glob = sorted([item for item in glob(str(path))])
-		else:
-			f_glob = sorted(
-				[item for name in file_glob for item in glob(name)])
+        print("Reading %d files..." % len(f_glob))
+        parsed = []
+        for filename in f_glob:
+            print(filename)
+            with Hdf(filename, "r") as in_file:
+                parse_data = np.concatenate(
+                    [
+                        self.parse_buffer(data)
+                        for buffer_type, utc, data in in_file
+                        if buffer_type == 12
+                    ]
+                )
+                parsed.append(parse_data)
+        self.numpy = np.concatenate(parsed)
+        self.series = pandas.DataFrame(self.numpy)
 
-		print("Reading %d files..." % len(f_glob))
-		parsed = []
-		for filename in f_glob:
-			print(filename)
-			with Hdf(filename, 'r') as in_file:
-				parse_data = np.concatenate([self.parse_buffer(data)
-						for buffer_type, utc, data in in_file
-						if buffer_type == 12])
-				parsed.append(parse_data)
-		self.numpy = np.concatenate(parsed)
-		self.series = pandas.DataFrame(self.numpy)
-
-
-
-	def parse_buffer(self, data):
-		headers_orig = data[::4]
-		osc_chan = data[1::4]
-		index = data[2::4]
-		adc_orig = data[3::4]
-		#Need to use numpy shift operators we overflow the 16-bit artihmetic before casting to 32
-		dt = (headers_orig >> 10) & 0x1
-		oscillator = np.right_shift((osc_chan & 0xfff0), 4, dtype=('uint32')) + \
-            np.left_shift((headers_orig & 0x00ff), 12, dtype=('uint32'))
-		#oscillator = ((osc_chan & 0xfff0) >> 4) + \
-		#    ((headers_orig & 0x00ff) << 12)
-		headers = (headers_orig >> 8)
-		asic = (osc_chan & 0x000f) >> 1
-		channel = ((osc_chan & 0x0001) << 4) + ((index & 0xf000) >> 12)
-		tac = ((index & 0x0fff) << 2) + ((adc_orig & 0xc000) >> 14)
-		adc = (adc_orig & 0x3fff)
-		np_array = np.core.records.fromarrays([
-			asic, channel, adc, tac, oscillator, headers, dt],
-			dtype=self.dtype)
-		ind = np.where(osc_chan != 0xffff)
-		return np_array[ind]
-
+    def parse_buffer(self, data):
+        headers_orig = data[::4]
+        osc_chan = data[1::4]
+        index = data[2::4]
+        adc_orig = data[3::4]
+        # Need to use numpy shift operators we overflow the 16-bit artihmetic before casting to 32
+        dt = (headers_orig >> 10) & 0x1
+        oscillator = np.right_shift(
+            (osc_chan & 0xFFF0), 4, dtype=("uint32")
+        ) + np.left_shift((headers_orig & 0x00FF), 12, dtype=("uint32"))
+        # oscillator = ((osc_chan & 0xfff0) >> 4) + \
+        #    ((headers_orig & 0x00ff) << 12)
+        headers = headers_orig >> 8
+        asic = (osc_chan & 0x000F) >> 1
+        channel = ((osc_chan & 0x0001) << 4) + ((index & 0xF000) >> 12)
+        tac = ((index & 0x0FFF) << 2) + ((adc_orig & 0xC000) >> 14)
+        adc = adc_orig & 0x3FFF
+        np_array = np.core.records.fromarrays(
+            [asic, channel, adc, tac, oscillator, headers, dt], dtype=self.dtype
+        )
+        ind = np.where(osc_chan != 0xFFFF)
+        return np_array[ind]
--- resource/hdf_to_roa/hdf_to_roa.py
+++ resource/hdf_to_roa/hdf_to_roa.py
@@ -2,84 +2,115 @@ from strip_map import StripMap
 from asic_convert import AsicConvert
 import os.path
 
-def openfile(filename, mode='r'):
-	if filename.endswith('.gz'):
-		import gzip
-		return gzip.open(filename, mode) 
-	else:
-		return open(filename, mode)
+
+def openfile(filename, mode="r"):
+    if filename.endswith(".gz"):
+        import gzip
+
+        return gzip.open(filename, mode)
+    else:
+        return open(filename, mode)
 
 
-def hdf_to_roa(hdf_file, stripmap_file,save_dir,save_name,verbose =False,gz=False,filter_side='both'):
-	
-	print("Reading Strip Map")
-	strip_map = StripMap(stripmap_file)
+def hdf_to_roa(
+    hdf_file,
+    stripmap_file,
+    save_dir,
+    save_name,
+    verbose=False,
+    gz=False,
+    filter_side="both",
+):
+    print("Reading Strip Map")
+    strip_map = StripMap(stripmap_file)
 
-	print("Reading HDF File")
-	if verbose: print (hdf_file)
-	d = AsicConvert([hdf_file],verbose=verbose)
-	asics = d.series.asic.max() + 1
+    print("Reading HDF File")
+    if verbose:
+        print(hdf_file)
+    d = AsicConvert([hdf_file], verbose=verbose)
+    asics = d.series.asic.max() + 1
 
-	print("Writing ROA File")
-	roa_save_dir =("/").join(hdf_file.split("/")[:-1]) 
-	if save_dir != None: roa_save_dir = save_dir.rstrip("/")
-	roa_save_name = hdf_file.split("/")[-1]
-	if save_name != None: roa_save_name = save_name
-	print (roa_save_dir + "/" + roa_save_name)
-	assert(os.path.exists(roa_save_dir))
-	
-	if gz:
-		opentype = 'wt'
-		filename = roa_save_dir + "/" + roa_save_name+".roa.gz"
-	else:
-		opentype = 'w'
-		filename = roa_save_dir + "/" + roa_save_name+".roa"
-	
-	with openfile(filename,opentype) as f:
-		#TODO: group events by oscillator
-		event_id = 0
+    print("Writing ROA File")
+    roa_save_dir = ("/").join(hdf_file.split("/")[:-1])
+    if save_dir != None:
+        roa_save_dir = save_dir.rstrip("/")
+    roa_save_name = hdf_file.split("/")[-1]
+    if save_name != None:
+        roa_save_name = save_name
+    print(roa_save_dir + "/" + roa_save_name)
+    assert os.path.exists(roa_save_dir)
 
-		#TODO: more than 1 detector
-		det = 0
+    if gz:
+        opentype = "wt"
+        filename = roa_save_dir + "/" + roa_save_name + ".roa.gz"
+    else:
+        opentype = "w"
+        filename = roa_save_dir + "/" + roa_save_name + ".roa"
 
-		f.write("TYPE ROA\n")
-		f.write("UF doublesidedstrip adc\n")
+    with openfile(filename, opentype) as f:
+        # TODO: group events by oscillator
+        event_id = 0
 
-		#d.numpy is a numpy record array representation of the data
-		#a record is (asic, channel, adc, tac, oscillator, headers) as defined in parse_asic.py
-		prev_osc = -1
-		offset=0
-		osc_freq = 200000.
-		for (asic, ch, adc, tac, oscillator, headers, dt) in d.numpy:
-			(side, strip) = strip_map.get_side_strip(asic, ch)
-			if side == 0:
-				side = 'p'
-				if filter_side == 'hv':
-					continue
-			else:
-				side = 'n'
-				if filter_side == 'lv':
-					continue
-			if oscillator != prev_osc:
-				if oscillator < prev_osc:
-					offset += 65536
-				f.write("\nSE\n")
-				f.write("CL {:f}\n".format((offset+oscillator)/osc_freq))
-				f.write("ID {}\n".format(event_id))
-				event_id+=1
-				prev_osc = oscillator
-				f.write("UH {} {} {} {} {}\n".format(det, strip, side, adc, tac))
+        # TODO: more than 1 detector
+        det = 0
+
+        f.write("TYPE ROA\n")
+        f.write("UF doublesidedstrip adc\n")
+
+        # d.numpy is a numpy record array representation of the data
+        # a record is (asic, channel, adc, tac, oscillator, headers) as defined in parse_asic.py
+        prev_osc = -1
+        offset = 0
+        osc_freq = 200000.0
+        for asic, ch, adc, tac, oscillator, headers, dt in d.numpy:
+            (side, strip) = strip_map.get_side_strip(asic, ch)
+            if side == 0:
+                side = "p"
+                if filter_side == "hv":
+                    continue
+            else:
+                side = "n"
+                if filter_side == "lv":
+                    continue
+            if oscillator != prev_osc:
+                if oscillator < prev_osc:
+                    offset += 65536
+                f.write("\nSE\n")
+                f.write("CL {:f}\n".format((offset + oscillator) / osc_freq))
+                f.write("ID {}\n".format(event_id))
+                event_id += 1
+                prev_osc = oscillator
+                f.write("UH {} {} {} {} {}\n".format(det, strip, side, adc, tac))
 
 
-if __name__ == "__main__": 
-	from argparse import ArgumentParser
-	p = ArgumentParser(description="temporary tool to convert hdf5 files to roa files that can be read in rrby nuclearizer") 
-	p.add_argument('--hdf', default=None, nargs='?',help = 'full path to hdf5 filename')
-	p.add_argument('--save_dir',default=None,help='directory in which to save the roa file, defaults to hdf directory')
-	p.add_argument('--save_name',default=None,help='file name for the roa file. Defaults to the same filename as the hdf file')
-	p.add_argument('--side', default='both')
-	p.add_argument('--stripmap',help = 'path to stripmap file')
-	p.add_argument("-v","--verbose", action = 'store_true')
-	p.add_argument('-gz', "--gz",default=False, action='store_true')	
-	args = p.parse_args()
-	hdf_to_roa(args.hdf, args.stripmap, args.save_dir,args.save_name,args.verbose,args.gz,args.side)
+if __name__ == "__main__":
+    from argparse import ArgumentParser
+
+    p = ArgumentParser(
+        description="temporary tool to convert hdf5 files to roa files that can be read in rrby nuclearizer"
+    )
+    p.add_argument("--hdf", default=None, nargs="?", help="full path to hdf5 filename")
+    p.add_argument(
+        "--save_dir",
+        default=None,
+        help="directory in which to save the roa file, defaults to hdf directory",
+    )
+    p.add_argument(
+        "--save_name",
+        default=None,
+        help="file name for the roa file. Defaults to the same filename as the hdf file",
+    )
+    p.add_argument("--side", default="both")
+    p.add_argument("--stripmap", help="path to stripmap file")
+    p.add_argument("-v", "--verbose", action="store_true")
+    p.add_argument("-gz", "--gz", default=False, action="store_true")
+    args = p.parse_args()
+    hdf_to_roa(
+        args.hdf,
+        args.stripmap,
+        args.save_dir,
+        args.save_name,
+        args.verbose,
+        args.gz,
+        args.side,
+    )
