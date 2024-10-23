8228ebed0d0afc4575fc139208dd28dcb1b13260
sophieehaight <144858596+sophieehaight@users.noreply.github.com>
2024-10-23 16:49:30 -0700

    v1

---
+++ src/MGUITACcut.cxx
@@ -0,0 +1,167 @@
+*
+ * MGUIOptionsEventSaver.cxx
+ *
+ *
+ * Copyright (C) by Andreas Zoglauer.
+ * All rights reserved.
+ *
+ *
+ * This code implementation is the intellectual property of
+ * Andreas Zoglauer.
+ *
+ * By copying, distributing or modifying the Program (or any work
+ * based on the Program) you indicate your acceptance of this statement,
+ * and all its terms.
+ *
+ */
+
+
+// Include the header:
+#include "MGUIOptionsEventSaver.h"
+
+// Standard libs:
+
+// ROOT libs:
+#include <TSystem.h>
+#include <MString.h>
+#include <TGLabel.h>
+#include <TGResourcePool.h>
+
+// MEGAlib libs:
+#include "MStreams.h"
+#include "MModuleEventSaver.h"
+
+
+////////////////////////////////////////////////////////////////////////////////
+
+
+#ifdef ___CLING___
+ClassImp(MGUIOptionsEventSaver)
+#endif
+
+
+////////////////////////////////////////////////////////////////////////////////
+
+
+MGUIOptionsEventSaver::MGUIOptionsEventSaver(MModule* Module) 
+  : MGUIOptions(Module)
+{
+  // standard constructor
+}
+
+
+////////////////////////////////////////////////////////////////////////////////
+
+
+MGUIOptionsEventSaver::~MGUIOptionsEventSaver()
+{
+  // kDeepCleanup is activated 
+}
+
+
+////////////////////////////////////////////////////////////////////////////////
+
+
+void MGUIOptionsEventSaver::Create()
+{
+  PreCreate();
+
+  TGLayoutHints* LabelLayout = new TGLayoutHints(kLHintsTop | kLHintsCenterX | kLHintsExpandX, 10, 10, 10, 10);  
+  
+  m_Mode = new MGUIERBList(m_OptionsFrame, "Please select an output mode:");
+  m_Mode->Add("*.roa file to use with melinator");
+  m_Mode->Add("*.dat file containing all information");
+  m_Mode->Add("*.evta file to use with revan");
+  m_Mode->SetSelected(dynamic_cast<MModuleEventSaver*>(m_Module)->GetMode());
+  m_Mode->Create();
+  m_OptionsFrame->AddFrame(m_Mode, LabelLayout);
+
+  
+  m_FileSelector = new MGUIEFileSelector(m_OptionsFrame, "Please select an output file:",
+  dynamic_cast<MModuleEventSaver*>(m_Module)->GetFileName());
+  m_FileSelector->SetFileType("roa file (read-out assemlies)", "*.roa");
+  m_FileSelector->SetFileType("dat file (all info)", "*.dat");
+  m_FileSelector->SetFileType("evta file (evta file)", "*.evta");
+  m_OptionsFrame->AddFrame(m_FileSelector, LabelLayout);
+
+  m_SaveBadEvents = new TGCheckButton(m_OptionsFrame, "Save events which are flagged bad (BD)", 1);
+  m_SaveBadEvents->SetOn(dynamic_cast<MModuleEventSaver*>(m_Module)->GetSaveBadEvents());
+  m_OptionsFrame->AddFrame(m_SaveBadEvents, LabelLayout);
+
+  m_AddTimeTag = new TGCheckButton(m_OptionsFrame, "Add a unique time tag", 3);
+  m_AddTimeTag->SetOn(dynamic_cast<MModuleEventSaver*>(m_Module)->GetAddTimeTag());
+  m_OptionsFrame->AddFrame(m_AddTimeTag, LabelLayout);
+
+  
+  m_SplitFile = new TGCheckButton(m_OptionsFrame, "Split the file", 2);
+  m_SplitFile->Associate(this);
+  m_SplitFile->SetOn(dynamic_cast<MModuleEventSaver*>(m_Module)->GetSplitFile());
+  m_OptionsFrame->AddFrame(m_SplitFile, LabelLayout);
+ 
+  TGLayoutHints* SplitFileTimeLayout = new TGLayoutHints(kLHintsTop | kLHintsCenterX | kLHintsExpandX, 30, 10, 0, 10);  
+  m_SplitFileTime = new MGUIEEntry(m_OptionsFrame, "Split the file after this time [sec]:", false, 
+    dynamic_cast<MModuleEventSaver*>(m_Module)->GetSplitFileTime().GetAsSystemSeconds(), true, 0l);
+  if (m_SplitFile->IsOn() == false) m_SplitFileTime->SetEnabled(false);
+  m_OptionsFrame->AddFrame(m_SplitFileTime, SplitFileTimeLayout);
+  
+  
+  PostCreate();
+}
+
+
+////////////////////////////////////////////////////////////////////////////////
+
+
+bool MGUIOptionsEventSaver::ProcessMessage(long Message, long Parameter1, long Parameter2)
+{
+  // Modify here if you have more buttons
+
+  bool Status = true;
+  
+  switch (GET_MSG(Message)) {
+  case kC_COMMAND:
+    switch (GET_SUBMSG(Message)) {
+    case kCM_BUTTON:
+      break;
+    case kCM_CHECKBUTTON:
+      if (Parameter1 == 2) {
+        m_SplitFileTime->SetEnabled(m_SplitFile->IsOn());
+      }
+      break;
+    default:
+      break;
+    }
+    break;
+  default:
+    break;
+  }
+  
+  if (Status == false) {
+    return false;
+  }
+
+  // Call also base class
+  return MGUIOptions::ProcessMessage(Message, Parameter1, Parameter2);
+}
+
+
+////////////////////////////////////////////////////////////////////////////////
+
+
+bool MGUIOptionsEventSaver::OnApply()
+{
+  // Modify this to store the data in the module!
+
+  dynamic_cast<MModuleEventSaver*>(m_Module)->SetMode(m_Mode->GetSelected());
+  dynamic_cast<MModuleEventSaver*>(m_Module)->SetFileName(m_FileSelector->GetFileName());
+  dynamic_cast<MModuleEventSaver*>(m_Module)->SetSaveBadEvents(m_SaveBadEvents->IsOn());
+  dynamic_cast<MModuleEventSaver*>(m_Module)->SetAddTimeTag(m_AddTimeTag->IsOn());
+  dynamic_cast<MModuleEventSaver*>(m_Module)->SetSplitFile(m_SplitFile->IsOn());
+  dynamic_cast<MModuleEventSaver*>(m_Module)->SetSplitFileTime(MTime(m_SplitFileTime->GetAsInt()));
+  
+  return true;
+}
+
+
+// MGUIOptionsEventSaver: the end...
+////////////////////////////////////////////////////////////////////////////////
---
+++ src/MModuleTACcut.cxx
@@ -0,0 +1,198 @@
+/*
+ * MModuleTACcut.cxx
+ *
+ *
+ * Copyright (C) by Andreas Zoglauer.
+ * All rights reserved.
+ *
+ *
+ * This code implementation is the intellectual property of
+ * Andreas Zoglauer.
+ *
+ * By copying, distributing or modifying the Program (or any work
+ * based on the Program) you indicate your acceptance of this statement,
+ * and all its terms.
+ *
+ */
+
+
+////////////////////////////////////////////////////////////////////////////////
+//
+// MModuleTACcut
+//
+////////////////////////////////////////////////////////////////////////////////
+
+
+// Include the header:
+#include "MModuleTACcut.h"
+
+// Standard libs:
+
+// ROOT libs:
+#include "TGClient.h"
+
+// MEGAlib libs:
+#include "MModule.h"
+#include "MGUIOptionsTACcut.h"
+
+
+////////////////////////////////////////////////////////////////////////////////
+
+
+#ifdef ___CLING___
+ClassImp(MModuleTACcut)
+#endif
+
+
+////////////////////////////////////////////////////////////////////////////////
+
+
+MModuleTACcut::MModuleTACcut() : MModule()
+{
+  // Construct an instance of MModuleTACcut
+
+  // Set all module relevant information
+
+  // Set the module name --- has to be unique
+  m_Name = "TACcut";
+
+  // Set the XML tag --- has to be unique --- no spaces allowed
+  m_XmlTag = "XmlTagTACcut";
+
+  // Set all modules, which have to be done before this module
+  AddPreceedingModuleType(MAssembly::c_DetectorEffectsEngine);
+  // AddPreceedingModuleType(MAssembly::c_EnergyCalibration);
+  // AddPreceedingModuleType(MAssembly::c_ChargeSharingCorrection);
+  // AddPreceedingModuleType(MAssembly::c_DepthCorrection);
+  // AddPreceedingModuleType(MAssembly::c_StripPairing);
+
+  // Set all types this modules handles
+  AddModuleType(MAssembly::c_DetectorEffectsEngine);
+
+
+  // Set all modules, which can follow this module
+
+  AddSucceedingModuleType(MAssembly::c_EventFilter)
+
+  // Set if this module has an options GUI
+  // Overwrite ShowOptionsGUI() with the call to the GUI!
+  m_HasOptionsGUI = true;
+  // If true, you have to derive a class from MGUIOptions (use MGUIOptionsTACcut)
+  // and implement all your GUI options
+
+  // Can the program be run multi-threaded
+  m_AllowMultiThreading = true;
+
+  // Can we use multiple instances of this class
+  m_AllowMultipleInstances = false;
+
+  //initialize a min and max TAC value 
+  m_MinimumTAC = 0;
+  m_MaximumTAC = 10000;
+}
+
+
+////////////////////////////////////////////////////////////////////////////////
+
+
+MModuleTACcut::~MModuleTACcut()
+{
+  // Delete this instance of MModuleTACcut
+}
+
+
+////////////////////////////////////////////////////////////////////////////////
+
+
+bool MModuleTACcut::Initialize()
+{
+  // Initialize the module 
+
+  return true;
+}
+
+
+////////////////////////////////////////////////////////////////////////////////
+
+
+bool MModuleTACcut::AnalyzeEvent(MReadOutAssembly* Event) 
+{
+  // Main data analysis routine, which updates the event to a new level 
+
+  // Apply cuts to the TAC values:
+   for (unsigned int i = 0; i < Event->GetNStripHits(); ) {
+    MStripHit* SH = Event->GetStripHit(i);
+    // takes inputted min and max TAC values from the GUI module to make cuts 
+    if ( SH->GetTiming() < m_MinimumTAC || SH->GetTiming() > m_MaximumTAC) {
+      //cout<<"HACK: Removing strip ht due to TAC "<<SH->GetTiming()<<" cut or energy "<<SH->GetEnergy()<<endl;
+      Event->RemoveStripHit(i);
+      delete SH;
+    } else {
+      ++i;
+    }
+  }
+  return true;
+}
+
+
+////////////////////////////////////////////////////////////////////////////////
+
+
+bool MModuleTACcut::Finalize()
+{
+  // Finalize the analysis - do all cleanup, i.e., undo Initialize() 
+
+  return true;
+}
+
+
+////////////////////////////////////////////////////////////////////////////////
+
+
+void MModuleTACcut::ShowOptionsGUI()
+{
+  //! Show the options GUI --- has to be overwritten!
+
+  MGUIOptionsTACcut* Options = new MGUIOptionsTACcut(this);
+  Options->Create();
+  gClient->WaitForUnmap(Options);
+}
+
+
+////////////////////////////////////////////////////////////////////////////////
+
+
+bool MModuleTACcut::ReadXmlConfiguration(MXmlNode* Node)
+{
+  //! Read the configuration data from an XML node
+
+  /*
+  MXmlNode* SomeTagNode = Node->GetNode("SomeTag");
+  if (SomeTagNode != 0) {
+    m_SomeTagValue = SomeTagNode->GetValue();
+  }
+  */
+
+  return true;
+}
+
+
+////////////////////////////////////////////////////////////////////////////////
+
+
+MXmlNode* MModuleTACcut::CreateXmlConfiguration() 
+{
+  //! Create an XML node tree from the configuration
+
+  MXmlNode* Node = new MXmlNode(0, m_XmlTag);
+  
+  /*
+  MXmlNode* SomeTagNode = new MXmlNode(Node, "SomeTag", "SomeValue");
+  */
+
+  return Node;
+}
+
+
+// MModuleTACcut.cxx: the end...
+////////////////////////////////////////////////////////////////////////////////
