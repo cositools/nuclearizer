5a4e10c5d024982ba1693052775c341bd0f3f13a
Felix Hagemann <hagemann@berkeley.edu>
2026-04-21 17:18:45 -0700

    Pass `m_ApplyResolutionCalibration` to `MSubModuleStripReadout`

--- src/MSubModuleDEEIntake.cxx
+++ src/MSubModuleDEEIntake.cxx
@@ -132,9 +132,10 @@ bool MSubModuleDEEIntake::AnalyzeEvent(MReadOutAssembly* Event)
     double DetectorDepth = Shape->GetSizeZ();
 
     MString DetectorName = Detector->GetName();
-    if (DetectorName.BeginsWith("GeD") == true) {
-      DetectorName.RemoveAllInPlace("GeD_"); // The number after GeD is the COSI detector ID
-      int DetectorID = DetectorName.ToInt();
+    if (DetectorName.BeginsWith("GeD") == true || DetectorName.BeginsWith("GuardRing") == true) {
+      // DetectorName.RemoveAllInPlace("GeD_"); // The number after GeD is the COSI detector ID
+      // int DetectorID = DetectorName.ToInt();
+      int DetectorID = 0;
 
       MDEEStripHit LVHit;
       MDEEStripHit HVHit;
