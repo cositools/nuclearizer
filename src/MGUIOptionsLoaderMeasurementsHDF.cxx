260fb35cf6e5ce73d8af1cfb1cf22c52475ddd15
sophieehaight <144858596+sophieehaight@users.noreply.github.com>
2026-06-02 09:24:10 -0400

    added CTD binning and photopeak fits to LV and HV data

--- src/MGUIOptionsLoaderMeasurementsHDF.cxx
+++ src/MGUIOptionsLoaderMeasurementsHDF.cxx
@@ -85,12 +85,6 @@ void MGUIOptionsLoaderMeasurementsHDF::Create()
     dynamic_cast<MModuleLoaderMeasurementsHDF*>(m_Module)->GetFileNameStripMap());
   m_FileSelectorStripMap->SetFileType("Strip map file", "*.map");
   m_OptionsFrame->AddFrame(m_FileSelectorStripMap, LabelLayout);
-  
-  // Nearest neighbor checkbox
-  m_IncludeNearestNeighbor = new TGCheckButton(m_OptionsFrame, "Include Nearest Neighbors");
-  m_IncludeNearestNeighbor->SetOn(dynamic_cast<MModuleLoaderMeasurementsHDF*>(m_Module)->GetIncludeNearestNeighbor());
-  m_IncludeNearestNeighbor->Associate(this);
-  m_OptionsFrame->AddFrame(m_IncludeNearestNeighbor, LabelLayout);
 
 
   PostCreate();
@@ -138,7 +132,6 @@ bool MGUIOptionsLoaderMeasurementsHDF::OnApply()
   dynamic_cast<MModuleLoaderMeasurementsHDF*>(m_Module)->SetFileName(m_FileSelectorHDF->GetFileName());
   dynamic_cast<MModuleLoaderMeasurementsHDF*>(m_Module)->SetLoadContinuationFiles(m_LoadContinuationFiles->IsOn());
   dynamic_cast<MModuleLoaderMeasurementsHDF*>(m_Module)->SetFileNameStripMap(m_FileSelectorStripMap->GetFileName());
-  dynamic_cast<MModuleLoaderMeasurementsHDF*>(m_Module)->SetIncludeNearestNeighbor(m_IncludeNearestNeighbor->IsOn());
 
   return true;
 }
