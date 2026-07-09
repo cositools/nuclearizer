8187914f1f790ccb69f3e7890e9abe196ee6bd64
sophieehaight <144858596+sophieehaight@users.noreply.github.com>
2026-07-09 11:30:41 -0700

    added GUI options for input param file

---
+++ include/MGUIOptionsTrappingCorrection.h
@@ -0,0 +1,121 @@
+/*
+ * MGUIOptionsTrappingCorrection.h
+ *
+ * Copyright (C) by Andreas Zoglauer.
+ * All rights reserved.
+ *
+ * Please see the source-file for the copyright-notice.:q
+ *
+ */
+
+
+#ifndef __MGUIOptionsTrappingCorrection__
+#define __MGUIOptionsTrappingCorrection__
+
+
+////////////////////////////////////////////////////////////////////////////////
+
+
+// ROOT libs:
+#include <TROOT.h>
+#include <TVirtualX.h>
+#include <TGWindow.h>
+#include <TObjArray.h>
+#include <TGFrame.h>
+#include <TGButton.h>
+#include <MString.h>
+#include <TGClient.h>
+
+// MEGAlib libs:
+#include "MGlobal.h"
+#include "MGUIEFileSelector.h"
+#include "MGUIOptions.h"
+#include "MGUIERBList.h"
+#include "MGUIEEntry.h"
+
+// Nuclearizer libs:
+#include "MModule.h"
+
+
+// Forward declarations:
+
+
+////////////////////////////////////////////////////////////////////////////////
+
+
+//! The user interface for the universal energy calibration
+class MGUIOptionsTrappingCorrection : public MGUIOptions
+{
+  // public Session:
+ public:
+  //! Default constructor
+  MGUIOptionsTrappingCorrection(MModule* Module);
+  //! Default destructor
+  virtual ~MGUIOptionsTrappingCorrection();
+
+  // //! Process all button, etc. messages
+  // virtual bool ProcessMessage(long Message, long Parameter1, long Parameter2);
+
+  //! The creation part which gets overwritten
+  virtual void Create();
+
+  // protected methods:
+ protected:
+
+  //! Actions after the Apply or OK button has been pressed
+  virtual bool OnApply();
+
+  // //! Toggle the radio buttons
+  // void ToggleRadioButtons(int WidgetID);
+
+
+  // protected members:
+ protected:
+
+  // private members:
+ private:
+
+  //! Select which file to load
+  MGUIEFileSelector* m_FileSelector;
+
+  // //! Radio button for ignoring the slow threshold cut
+  // TGRadioButton* m_SlowThresholdCutRBIgnore;
+
+  // //! Radio button for using a fixed slow threshold cut
+  // TGRadioButton* m_SlowThresholdCutRBFixed;
+  // //! The slow threshold cut entry box
+  // MGUIEEntry* m_SlowThresholdCutFixedValue;
+
+  // //! Radio button for reading slow threshold cuts from a file
+  // TGRadioButton* m_SlowThresholdCutRBFile;
+  // //! The slow threshold cut file selector
+  // MGUIEFileSelector* m_SlowThresholdCutFileSelector;
+  
+  // //! Radio button for ignoring the threshold cut for Nearest Neighbors
+  // TGRadioButton* m_SlowThresholdCutNearestNeighborRBIgnore;
+  // //! Radio button for using a fixed threshold cut for Nearest Neighbors
+  // TGRadioButton* m_SlowThresholdCutNearestNeighborRBFixed;
+  // //! The threshold cut entry box for Nearest Neighbors
+  // MGUIEEntry* m_SlowThresholdCutNearestNeighborFixedValue;
+
+
+  // //! IDs of radio buttons
+  // //! The numbers should be identical to MSlowThresholdCutModes
+  // enum RBButtonIDs { c_SlowThresholdIgnore = 0,
+  //   c_SlowThresholdFixed,
+  //   c_SlowThresholdFile,
+  //   c_SlowThresholdNearestNeighborIgnore = 100,
+  //   c_SlowThresholdNearestNeighborFixed};
+
+
+#ifdef ___CLING___
+ public:
+  ClassDef(MGUIOptionsTrappingCorrection, 1)
+#endif
+
+};
+
+#endif
+
+
+////////////////////////////////////////////////////////////////////////////////
---
+++ src/MGUIOptionsTrappingCorrection.cxx
@@ -0,0 +1,250 @@
+/*
+ * MGUIOptionsTrappingCorrection.cxx
+ *
+ *
+ * Copyright (C) by Andreas Zoglauer.
+ * All rights reserved.
+ *
+ *
+ * This code implementation is the intellectual property of
+ * Andreas Zoglauer.
+ *
+ * By copying, distributing or modifying the Program (or any work
+ * based on the Program) you indicate your acceptance of this statement,
+ * and all its terms.
+ *
+ */
+
+
+// Include the header:
+#include "MGUIOptionsTrappingCorrection.h"
+
+// Standard libs:
+
+// ROOT libs:
+#include <TSystem.h>
+#include <MString.h>
+#include <TGLabel.h>
+#include <TGResourcePool.h>
+
+// MEGAlib libs:
+#include "MStreams.h"
+#include "MModule.h"
+#include "MModuleTrappingCorrection.h"
+
+
+////////////////////////////////////////////////////////////////////////////////
+
+
+#ifdef ___CLING___
+ClassImp(MGUIOptionsTrappingCorrection)
+#endif
+
+
+////////////////////////////////////////////////////////////////////////////////
+
+
+MGUIOptionsTrappingCorrection::MGUIOptionsTrappingCorrection(MModule* Module) 
+  : MGUIOptions(Module)
+{
+  // standard constructor
+}
+
+
+////////////////////////////////////////////////////////////////////////////////
+
+
+MGUIOptionsTrappingCorrection::~MGUIOptionsTrappingCorrection()
+{
+  // kDeepCleanup is activated 
+}
+
+
+////////////////////////////////////////////////////////////////////////////////
+
+
+void MGUIOptionsTrappingCorrection::Create()
+{
+  PreCreate();
+
+  TGLayoutHints* LabelLayout = new TGLayoutHints(kLHintsTop | kLHintsLeft, 10, 10, 10, 10);
+  TGLayoutHints* FileSelectorLayout = new TGLayoutHints(kLHintsTop | kLHintsLeft | kLHintsExpandX, 10, 10, 10, 10);
+
+  // File loader for energy calibration file
+  m_FileSelector = new MGUIEFileSelector(m_OptionsFrame, "Please select a trapping parameter file:",
+    dynamic_cast<MModuleTrappingCorrection*>(m_Module)->GetFileName());
+  m_FileSelector->SetFileType("Trapping parameter file", "*.csv");
+  m_OptionsFrame->AddFrame(m_FileSelector, FileSelectorLayout);
+
+
+  // TGLabel* SlowThresholdLabel = new TGLabel(m_OptionsFrame, "Please choose how to handle the slow threshold cut:");
+  // m_OptionsFrame->AddFrame(SlowThresholdLabel, LabelLayout);
+
+
+  // TGLayoutHints* RBLayout = new TGLayoutHints(kLHintsLeft | kLHintsTop, 40, 10, 2, 0);
+  // TGLayoutHints* RBOptionLayout = new TGLayoutHints(kLHintsLeft | kLHintsTop, 60, 10, 2, 0);
+  // TGLayoutHints* RBOptionStretchLayout = new TGLayoutHints(kLHintsLeft | kLHintsTop | kLHintsExpandX, 60, 10, 2, 0);
+
+  // m_SlowThresholdCutRBIgnore = new TGRadioButton(m_OptionsFrame, "Do not apply a slow threshold cut", c_SlowThresholdIgnore);
+  // m_SlowThresholdCutRBIgnore->Associate(this);
+  // m_OptionsFrame->AddFrame(m_SlowThresholdCutRBIgnore, RBLayout);
+
+
+  // m_SlowThresholdCutRBFixed = new TGRadioButton(m_OptionsFrame, "Use one uniform slow threshold cut for all strips", c_SlowThresholdFixed);
+  // m_SlowThresholdCutRBFixed->Associate(this);
+  // m_OptionsFrame->AddFrame(m_SlowThresholdCutRBFixed, RBLayout);
+
+  // m_SlowThresholdCutFixedValue = new MGUIEEntry(m_OptionsFrame, "Set threshold value [keV]:", false, dynamic_cast<MModuleTrappingCorrection*>(m_Module)->GetSlowThresholdCutFixedValue(), true, 0.0);
+  // m_OptionsFrame->AddFrame(m_SlowThresholdCutFixedValue, RBOptionLayout);
+
+
+  // m_SlowThresholdCutRBFile = new TGRadioButton(m_OptionsFrame, "Read slow threshold cuts from file (unique to each strip)", c_SlowThresholdFile);
+  // m_SlowThresholdCutRBFile->Associate(this);
+  // m_OptionsFrame->AddFrame(m_SlowThresholdCutRBFile, RBLayout);
+
+  // m_SlowThresholdCutFileSelector = new MGUIEFileSelector(m_OptionsFrame, "", dynamic_cast<MModuleTrappingCorrection*>(m_Module)->GetSlowThresholdCutFileName());
+  // m_SlowThresholdCutFileSelector->SetFileType("Slow threshold cut per strip file", "*.csv");
+  // m_OptionsFrame->AddFrame(m_SlowThresholdCutFileSelector, RBOptionStretchLayout);
+
+
+  // // Toggle the right button
+  // MSlowThresholdCutModes SlowThresholdCutMode = dynamic_cast<MModuleTrappingCorrection*>(m_Module)->GetSlowThresholdCutMode();
+  // ToggleRadioButtons(static_cast<int>(SlowThresholdCutMode));
+  
+  
+  // // Nearest Neighbor threshold cuts!
+  // TGLabel* NearestNeighborLabel = new TGLabel(m_OptionsFrame, "Please choose how to handle the threshold cut for Nearest Neighbors:");
+  // m_OptionsFrame->AddFrame(NearestNeighborLabel, LabelLayout);
+
+  // m_SlowThresholdCutNearestNeighborRBIgnore = new TGRadioButton(m_OptionsFrame, "Do not apply a slow threshold cut to Nearest Neighbors", c_SlowThresholdNearestNeighborIgnore);
+  // m_SlowThresholdCutNearestNeighborRBIgnore->Associate(this);
+  // m_OptionsFrame->AddFrame(m_SlowThresholdCutNearestNeighborRBIgnore, RBLayout);
+      
+  // m_SlowThresholdCutNearestNeighborRBFixed = new TGRadioButton(m_OptionsFrame, "Use one uniform slow threshold cut for all Nearest Neighbors", c_SlowThresholdNearestNeighborFixed);
+  // m_SlowThresholdCutNearestNeighborRBFixed->Associate(this);
+  // m_OptionsFrame->AddFrame(m_SlowThresholdCutNearestNeighborRBFixed, RBLayout);
+      
+  // m_SlowThresholdCutNearestNeighborFixedValue = new MGUIEEntry(m_OptionsFrame, "Set threshold value [keV] for Nearest Neighbors:", false, dynamic_cast<MModuleTrappingCorrection*>(m_Module)->GetNearestNeighborThreshold());
+  // m_OptionsFrame->AddFrame(m_SlowThresholdCutNearestNeighborFixedValue, RBOptionLayout);
+  
+  // MNearestNeighborCutModes NearestNeighborCutMode = dynamic_cast<MModuleTrappingCorrection*>(m_Module)->GetNearestNeighborCutMode();
+  // ToggleRadioButtons(static_cast<int>(NearestNeighborCutMode));
+    
+
+  // PostCreate();
+}
+
+
+////////////////////////////////////////////////////////////////////////////////
+
+
+// bool MGUIOptionsTrappingCorrection::ProcessMessage(long Message, long Parameter1, long Parameter2)
+// {
+//   // Modify here if you have more buttons
+
+//   bool Status = true;
+
+//   switch (GET_MSG(Message)) {
+//   case kC_COMMAND:
+//     switch (GET_SUBMSG(Message)) {
+//     case kCM_BUTTON:
+//       break;
+//     case kCM_RADIOBUTTON:
+//       ToggleRadioButtons(Parameter1);
+//       break;
+//     case kCM_CHECKBUTTON:
+//       break;
+//     default:
+//       break;
+//     }
+//     break;
+//   default:
+//     break;
+//   }
+  
+//   if (Status == false) {
+//     return false;
+//   }
+
+//   // Call also base class
+//   return MGUIOptions::ProcessMessage(Message, Parameter1, Parameter2);
+// }
+
+
+////////////////////////////////////////////////////////////////////////////////
+
+
+// void MGUIOptionsTrappingCorrection::ToggleRadioButtons(int WidgetID)
+// {
+//   // Toggle the radio buttons and the entry fields
+
+//   if (WidgetID == m_SlowThresholdCutRBIgnore->WidgetId()) {
+//     m_SlowThresholdCutRBIgnore->SetState(kButtonDown);
+//     m_SlowThresholdCutRBFixed->SetState(kButtonUp);
+//     m_SlowThresholdCutFixedValue->SetEnabled(false);
+//     m_SlowThresholdCutRBFile->SetState(kButtonUp);
+//     m_SlowThresholdCutFileSelector->SetEnabled(false);
+//   } else if (WidgetID == m_SlowThresholdCutRBFixed->WidgetId()) {
+//     m_SlowThresholdCutRBIgnore->SetState(kButtonUp);
+//     m_SlowThresholdCutRBFixed->SetState(kButtonDown);
+//     m_SlowThresholdCutFixedValue->SetEnabled(true);
+//     m_SlowThresholdCutRBFile->SetState(kButtonUp);
+//     m_SlowThresholdCutFileSelector->SetEnabled(false);
+//   } else if (WidgetID == m_SlowThresholdCutRBFile->WidgetId()) {
+//     m_SlowThresholdCutRBIgnore->SetState(kButtonUp);
+//     m_SlowThresholdCutRBFixed->SetState(kButtonUp);
+//     m_SlowThresholdCutFixedValue->SetEnabled(false);
+//     m_SlowThresholdCutRBFile->SetState(kButtonDown);
+//     m_SlowThresholdCutFileSelector->SetEnabled(true);
+//   }
+  
+//   // Nearest Neighbors
+//   if (WidgetID == c_SlowThresholdNearestNeighborIgnore || WidgetID == c_SlowThresholdNearestNeighborFixed) {
+//     if (WidgetID == c_SlowThresholdNearestNeighborIgnore) {
+//       m_SlowThresholdCutNearestNeighborRBIgnore->SetState(kButtonDown);
+//       m_SlowThresholdCutNearestNeighborRBFixed->SetState(kButtonUp);
+//       m_SlowThresholdCutNearestNeighborFixedValue->SetEnabled(false);
+//     } else if (WidgetID == c_SlowThresholdNearestNeighborFixed) {
+//       m_SlowThresholdCutNearestNeighborRBIgnore->SetState(kButtonUp);
+//       m_SlowThresholdCutNearestNeighborRBFixed->SetState(kButtonDown);
+//       m_SlowThresholdCutNearestNeighborFixedValue->SetEnabled(true);
+//     }
+//   }
+// }
+
+
+////////////////////////////////////////////////////////////////////////////////
+
+
+bool MGUIOptionsTrappingCorrection::OnApply()
+{
+ // Modify this to store the data in the module!
+
+  dynamic_cast<MModuleTrappingCorrection*>(m_Module)->SetFileName(m_FileSelector->GetFileName());
+
+  // if (m_SlowThresholdCutRBIgnore->GetState() == kButtonDown) {
+  //   dynamic_cast<MModuleTrappingCorrection*>(m_Module)->SetSlowThresholdCutMode(MSlowThresholdCutModes::e_Ignore);
+  // } else if (m_SlowThresholdCutRBFixed->GetState() == kButtonDown) {
+  //   dynamic_cast<MModuleTrappingCorrection*>(m_Module)->SetSlowThresholdCutMode(MSlowThresholdCutModes::e_Fixed);
+  // }  if (m_SlowThresholdCutRBFile->GetState() == kButtonDown) {
+  //   dynamic_cast<MModuleTrappingCorrection*>(m_Module)->SetSlowThresholdCutMode(MSlowThresholdCutModes::e_File);
+  // }
+
+  // dynamic_cast<MModuleTrappingCorrection*>(m_Module)->SetSlowThresholdCutFixedValue(m_SlowThresholdCutFixedValue->GetAsDouble());
+  // dynamic_cast<MModuleTrappingCorrection*>(m_Module)->SetSlowThresholdCutFileName(m_SlowThresholdCutFileSelector->GetFileName());
+  
+  // // Nearest Neighbors
+  // if (m_SlowThresholdCutNearestNeighborRBIgnore->GetState() == kButtonDown) {
+  //   dynamic_cast<MModuleTrappingCorrection*>(m_Module)->SetNearestNeighborCutMode(MNearestNeighborCutModes::e_Ignore);
+  // } else {
+  //   dynamic_cast<MModuleTrappingCorrection*>(m_Module)->SetNearestNeighborCutMode(MNearestNeighborCutModes::e_Fixed);
+  // }
+
+  // dynamic_cast<MModuleTrappingCorrection*>(m_Module)->SetNearestNeighborThreshold(m_SlowThresholdCutNearestNeighborFixedValue->GetAsDouble());
+
+  return true;
+}
+
+
+// MGUIOptionsTrappingCorrection: the end...
+////////////////////////////////////////////////////////////////////////////////
