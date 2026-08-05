dc4041801456a72af77b39af1cbcf72156d320fd
Julian Gerber <jmgerber@ucsd.edu>
2026-08-05 14:34:11 -0700

    Revert "Merge remote-tracking branch 'carolyn/qa_option'"
    This reverts commit 9f7b81318957fc8d8d7c338000a5e1af7c961673, reversing
    changes made to 2879859447d4a1975da0f8b76e6af9f0189af928.

--- include/MGUIOptionsEventSaver.h
+++ include/MGUIOptionsEventSaver.h
@@ -81,9 +81,6 @@ class MGUIOptionsEventSaver : public MGUIOptions
   //! Checkbutton to save or reject bad events
   TGCheckButton* m_SaveBadEvents;
 
-  //! Checkbutton to save or reject quality flag events
-  TGCheckButton* m_SavePoorQualityEvents;
-
   //! Checkbutton to save veto events
   TGCheckButton* m_SaveVetoEvents;
 
--- src/MGUIOptionsEventSaver.cxx
+++ src/MGUIOptionsEventSaver.cxx
@@ -108,10 +108,6 @@ void MGUIOptionsEventSaver::Create()
   m_SaveBadEvents->SetOn(dynamic_cast<MModuleEventSaver*>(m_Module)->GetSaveBadEvents());
   GeneralFrame->AddFrame(m_SaveBadEvents, FirstLabelLayout);
 
-  m_SavePoorQualityEvents = new TGCheckButton(GeneralFrame, "Save events with quality flag (QA)", 1);
-  m_SavePoorQualityEvents->SetOn(dynamic_cast<MModuleEventSaver*>(m_Module)->GetSavePoorQualityEvents());
-  GeneralFrame->AddFrame(m_SavePoorQualityEvents, TightButtonLayout);
-
   m_SaveVetoEvents = new TGCheckButton(GeneralFrame, "Save guard ring and shield veto events (Veto)", 1);
   m_SaveVetoEvents->SetOn(dynamic_cast<MModuleEventSaver*>(m_Module)->GetSaveVetoEvents());
   GeneralFrame->AddFrame(m_SaveVetoEvents, TightButtonLayout);
@@ -233,7 +229,6 @@ bool MGUIOptionsEventSaver::OnApply()
   dynamic_cast<MModuleEventSaver*>(m_Module)->SetFileName(m_FileSelector->GetFileName());
 
   dynamic_cast<MModuleEventSaver*>(m_Module)->SetSaveBadEvents(m_SaveBadEvents->IsOn());
-  dynamic_cast<MModuleEventSaver*>(m_Module)->SetSavePoorQualityEvents(m_SavePoorQualityEvents->IsOn());
   dynamic_cast<MModuleEventSaver*>(m_Module)->SetSaveVetoEvents(m_SaveVetoEvents->IsOn());
   dynamic_cast<MModuleEventSaver*>(m_Module)->SetAddTimeTag(m_AddTimeTag->IsOn());
   dynamic_cast<MModuleEventSaver*>(m_Module)->SetSplitFile(m_SplitFile->IsOn());
