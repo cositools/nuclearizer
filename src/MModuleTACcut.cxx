b4b68b4826b8e6b46811e96d3296a55449f8bb96
nicolerodriguezcavero <nrodriguezcavero@berkeley.edu>
2026-08-13 11:33:12 -0700

    Requiring Ecal preceeding module

--- src/MModuleTACcut.cxx
+++ src/MModuleTACcut.cxx
@@ -64,7 +64,7 @@ MModuleTACcut::MModuleTACcut() : MModule()
 
   // Set all modules, which have to be done before this module
   AddPreceedingModuleType(MAssembly::c_EventLoader);
-  //AddPreceedingModuleType(MAssembly::c_EnergyCalibration);
+  AddPreceedingModuleType(MAssembly::c_EnergyCalibration);
 
   // Set all types this modules handles
   AddModuleType(MAssembly::c_TACcut);
