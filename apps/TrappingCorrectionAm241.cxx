d8e1b98f47a3d550f06297d437252464ce9592d9
sophieehaight <144858596+sophieehaight@users.noreply.github.com>
2026-08-20 14:59:45 -0700

    cout cleanup

--- apps/TrappingCorrectionAm241.cxx
+++ apps/TrappingCorrectionAm241.cxx
@@ -274,16 +274,13 @@ bool TrappingCorrectionAm241::Analyze()
   // Store the HV and LV input files
   vector<MString> FileNames;
   FileNames.push_back(m_HVFileName);
-  cout<<"HV file names stored"<<endl;
   FileNames.push_back(m_LVFileName);
-  cout<<"LV file names stored"<<endl;
 
   // Map the side integer to HV and LV labels
   // i.e. 0=HV, 1=LV
   vector<MString> IllumSide;
   IllumSide.push_back(MString("HV"));
   IllumSide.push_back(MString("LV"));
-  cout<<"HV and LV integers mapped"<<endl;
 
   // Make a directory in which to store the pixel-level data 
   MString PixelDir = m_OutFile + MString("_pixeldata");
@@ -320,7 +317,6 @@ bool TrappingCorrectionAm241::Analyze()
     // Read in the input files and make a list of hdf5 files to calibrate
     if ((InputFile.GetSubString(InputFile.Length() - 4)) == "hdf5") {
       HDFNames.push_back(InputFile);
-      cout<<"hdf names loaded correctly"<<endl;
     } else if ((InputFile.GetSubString(InputFile.Length() - 3)) == "txt") {
       cout<<"Reading input file "<<InputFile<<endl;
       cout<<"WARNING: When passing a list of files, ensure that you have chosen the correct HDF5 continuous reading mode. Use the --nocontinue option to suppress continuous file reading."<<endl;
@@ -348,7 +344,6 @@ bool TrappingCorrectionAm241::Analyze()
     for (unsigned int f = 0; f<HDFNames.size(); ++f) {
 
       MString File = HDFNames[f];
-      cout<<"Beginning analysis of file "<<File<<endl;
 
       // Create and initialize nuclearizer modules
       MSupervisor* S = MSupervisor::GetSupervisor();
@@ -359,7 +354,6 @@ bool TrappingCorrectionAm241::Analyze()
     	MModuleEventFilter* EventFilter;
 
       unsigned int MNumber = 0;
-      cout<<"Creating HDF5 loader"<<endl;
       Loader = new MModuleLoaderMeasurementsHDF();
       Loader->SetFileNameStripMap(m_StripMapFile);
       Loader->SetFileName(File);
@@ -367,21 +361,18 @@ bool TrappingCorrectionAm241::Analyze()
       S->SetModule(Loader, MNumber);
       ++MNumber;
 
-      cout<<"Creating TAC calibrator"<<endl;
       TACCalibrator = new MModuleTACcut();
       TACCalibrator->SetTACCalFileName(m_TACCalFile);
       TACCalibrator->SetTACCutFileName(m_TACCutFile);
       S->SetModule(TACCalibrator, MNumber);
       ++MNumber;
      
-      cout<<"Creating energy calibrator"<<endl;
       EnergyCalibrator = new MModuleEnergyCalibration();
       EnergyCalibrator->SetFileName(m_EcalFile);
       //EnergyCalibrator->EnablePreampTempCorrection(false);
       S->SetModule(EnergyCalibrator, MNumber);
       ++MNumber;
 
-      cout<<"Creating Event filter"<<endl;
       // Only use events with 1 to 3 Strip Hits on each side
       // After strip pairing, we'll also filter to make sure we're only looking at single Hits
       EventFilter = new MModuleEventFilter();
@@ -395,8 +386,7 @@ bool TrappingCorrectionAm241::Analyze()
       EventFilter->SetMaximumTotalEnergy(m_MaxEnergy*2); // Multiply by 2 because this is the event-level energy, i.e. sum over both sides
       S->SetModule(EventFilter, MNumber);
       ++MNumber;
-      
-      cout<<"Creating strip pairing"<<endl;
+
       MModule* Pairing;
       if (m_MultiRoundStripPairing == true) {
         Pairing = new MModuleStripPairingMultiRoundChiSquare();
@@ -405,22 +395,16 @@ bool TrappingCorrectionAm241::Analyze()
       }
       S->SetModule(Pairing, MNumber);
 
-      cout<<"Initializing Loader"<<endl;
       if (Loader->Initialize() == false) return false;
-      cout<<"Initializing TAC calibrator"<<endl;
       if (TACCalibrator->Initialize() == false) return false;
-      cout<<"Initializing Energy calibrator"<<endl;
       if (EnergyCalibrator->Initialize() == false) return false;
-      cout<<"Initializing Event filter"<<endl;
       if (EventFilter->Initialize() == false) return false;
-      cout<<"Initializing Pairing"<<endl;
       if (Pairing->Initialize() == false) return false;
 
       bool IsFinished = false;
       MReadOutAssembly* Event = new MReadOutAssembly();
 
       // Pass Events through each module. Once calibrated, add the Event to the histograms
-      cout<<"Analyzing..."<<endl;
       while ((IsFinished == false) && (m_Interrupt == false)) {
         Event->Clear();
 
