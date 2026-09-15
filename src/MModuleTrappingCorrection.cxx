9a188634326420552c335eba1075b0d55cbbeeda
sophieehaight <144858596+sophieehaight@users.noreply.github.com>
2026-09-15 15:05:42 -0700

    changed continue to false for GR events + added verbosity check to outputs

--- src/MModuleTrappingCorrection.cxx
+++ src/MModuleTrappingCorrection.cxx
@@ -154,7 +154,7 @@ bool MModuleTrappingCorrection::AnalyzeEvent(MReadOutAssembly* Event)
 {
 
   if (Event->GetGuardRingVeto() || m_DetectorParamMap.empty()) {
-    continue;
+    return false;
   } 
 
   for (unsigned int i = 0; i < Event->GetNHits(); ++i) {
